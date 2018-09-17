ba8fe54cdbb6b5c49cef818601866a74fbf659a3
87627 <876271327@qq.com>
2018-09-17 08:12:12 +0800

    动效-风扇吹动叶子的Loading

--- animator-demo/src/main/java/cn/fortrun/animator/custom/RoundAvatarView.java
+++ animator-demo/src/main/java/cn/fortrun/animator/custom/RoundAvatarView.java
@@ -1,10 +1,21 @@
 package cn.fortrun.animator.custom;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ComposeShader;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import cn.fortrun.animator.R;
 
 /**
  * description
@@ -15,25 +26,46 @@ import android.view.View;
  * @since 1.0.0
  */
 public class RoundAvatarView extends View {
+    private Paint mBitmapPaint = new Paint();
+    private int mTotalWidth = 600;
+    private float mHalfWidth = mTotalWidth / 2;
+    private Bitmap mBitmap;
+
+    private static final int[] mColors = new int[]{
+            Color.TRANSPARENT, Color.TRANSPARENT, Color.WHITE
+    };
+    private static final float[] mPositions = new float[]{
+            0, 0.6f, 1f
+    };
+
     public RoundAvatarView(Context context) {
         super(context);
     }
 
     public RoundAvatarView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        //创建位图
+        mBitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.simpson)).getBitmap();
+        //将图scale成我们想要的大小
+        mBitmap = Bitmap.createScaledBitmap(mBitmap, mTotalWidth, mTotalWidth, false);
+
+        // 创建位图渲染
+        BitmapShader bitmapShader = new BitmapShader(mBitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        // 创建环形渐变
+        RadialGradient radialGradient = new RadialGradient(mHalfWidth, mHalfWidth, mHalfWidth,
+                mColors, mPositions, Shader.TileMode.MIRROR);
+        // 创建组合渐变，由于直接按原样绘制就好，所以选择Mode.SRC_OVER
+        ComposeShader composeShader = new ComposeShader(bitmapShader, radialGradient,
+                PorterDuff.Mode.SRC_OVER);
+        // 将位图渲染设置给paint
+        mBitmapPaint.setShader(composeShader);
     }
 
-    public RoundAvatarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvas.drawCircle(mHalfWidth, mHalfWidth, mHalfWidth, mBitmapPaint);
     }
 }
---
+++ animator-demo/src/main/java/cn/fortrun/animator/custom/UiUtils.java
@@ -0,0 +1,37 @@
+package cn.fortrun.animator.custom;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * description
+ *
+ * @author 87627
+ * @create 2018.09.16 17:01
+ * @since 1.0.0
+ */
+public class UiUtils {
+    static public int getScreenWidthPixels(Context context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                .getMetrics(dm);
+        return dm.widthPixels;
+    }
+
+    static public int dipToPx(Context context, int dip) {
+        return (int) (dip * getScreenDensity(context) + 0.5f);
+    }
+
+    static public float getScreenDensity(Context context) {
+        try {
+            DisplayMetrics dm = new DisplayMetrics();
+            ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                    .getMetrics(dm);
+            return dm.density;
+        } catch (Exception e) {
+            return DisplayMetrics.DENSITY_DEFAULT;
+        }
+    }
+
+}
